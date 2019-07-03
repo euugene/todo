ad4f1742bf3a0119eee74816264e921226358118
Eugene <j.gashuck@gmail.com>
2019-07-03 19:48:19 +0300

    Updates

---
+++ precache-manifest.ddb0575be77a9dd2f4177a12fc7778d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4a60e5915cbaf5c552128c7573ff47",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "ef39668191c69d760498",
+    "url": "/todo/static/css/main.1754e9eb.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/todo/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "ef39668191c69d760498",
+    "url": "/todo/static/js/main.1b55c8aa.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
